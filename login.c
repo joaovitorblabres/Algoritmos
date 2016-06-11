b819a5fd1f122538257aca23dcc04128f5482601
joaovitorblabres <joaovitorlabres@gmail.com>
2016-06-10 23:16:00 -0300

    att insert usuario

---
+++ login.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int form();
+
+int form(){
+	char login[100];
+	char senha[100];
+
+	printf("Informe seu login: ");
+	fgets(login,100,stdin);		
+	login[strlen(login)-1] = '\0';
+	printf("Informe sua senha: ");
+	fgets(senha,100,stdin);		
+	senha[strlen(senha)-1] = '\0';
+	
+	
+
+}
